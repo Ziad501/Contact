13aceba019655e04e798d0f4d00b2816279609a1
AbdallahSoliman22 <bedoibrahim20@gmail.com>
2025-02-24 14:27:19 +0200

    Added Logged in form

--- WinFormsApp2/Forms/Loggedin.cs
+++ WinFormsApp2/Forms/Loggedin.cs
@@ -2,5 +2,32 @@
 {
     public partial class Loggedin : Form
     {
-    }
+		public static class SessionManager
+		{
+			public static string LoggedInUsername { get; set; }
+		}
+
+		private string _loggedInUsername;
+
+		public Loggedin(MainLoginform mainloginForm)
+		{
+			InitializeComponent();
+			mainloginForm.UserLoggedIn += LoginForm_UserLoggedIn;
+		}
+
+		private void LoginForm_UserLoggedIn(string username)
+		{
+			SessionManager.LoggedInUsername = username;
+			lblWelcome.Text = $"Welcome, {SessionManager.LoggedInUsername}!";
+		}
+
+
+		private void Loggedin_Load(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(SessionManager.LoggedInUsername))
+			{
+				lblWelcome.Text = $"Welcome, {SessionManager.LoggedInUsername}!";
+			}
+		}
+	}
 }
