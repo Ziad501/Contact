a782ef1b986ca2b1ac1848bedd140fd75fed7595
AbdallahSoliman22 <bedoibrahim20@gmail.com>
2025-02-24 14:13:09 +0200

    Added EmployeeService (CRUD)

--- WinFormsApp2.BLL/Services/EmployeeService.cs
+++ WinFormsApp2.BLL/Services/EmployeeService.cs
@@ -3,4 +3,40 @@ using WinFormsApp2.DAL.Entities;
 
 namespace WinFormsApp2.BLL.Services
 {
+	public class EmployeeService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public EmployeeService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public List<Employee> GetAllEmployees()
+		{
+			return _context.Employees.ToList();
+		}
+
+		public void AddEmployee(Employee employee)
+		{
+			_context.Employees.Add(employee);
+			_context.SaveChanges();
+		}
+
+		public void UpdateEmployee(Employee employee)
+		{
+			_context.Employees.Update(employee);
+			_context.SaveChanges();
+		}
+
+		public void DeleteEmployee(int id)
+		{
+			var employee = _context.Employees.Find(id);
+			if (employee != null)
+			{
+				_context.Employees.Remove(employee);
+				_context.SaveChanges();
+			}
+		}
+	}
 }
